0e6fb0e2c02d9568d5ec8add557e6dcdf817c17b
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2024-11-11 15:33:25 +0200

    refactor build.js for improved readability and error handling

--- .github/scripts/build.js
+++ .github/scripts/build.js
@@ -8,35 +8,19 @@ const tpl = getTemplate(TPL_FILE);
 
 const categories = {};
 
-data.repositories.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameA > nameB) {
-        return 1;
-    }
-    return 0;
-}).forEach(repo =>
-    repo.technologies.forEach(tech => {
-        if (!categories.hasOwnProperty(tech)) {
-            categories[tech] = [];
-        }
-        categories[tech].push(repo);
-    }))
+data.repositories.sort((a, b) => a.name.localeCompare(b.name))
+    .forEach(repo => {
+        repo.technologies.forEach(tech => {
+            if (!categories.hasOwnProperty(tech)) {
+                categories[tech] = [];
+            }
+            categories[tech].push(repo);
+        });
+    });
 
-const sortedCategories = Object.fromEntries(Object.entries(categories).sort((a, b) => {
-    const nameA = a[0].toUpperCase();
-    const nameB = b[0].toUpperCase();
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameA > nameB) {
-        return 1;
-    }
-    return 0;
-}));
+const sortedCategories = Object.fromEntries(
+    Object.entries(categories).sort((a, b) => a[0].localeCompare(b[0]))
+);
 
 const toc = Object.keys(sortedCategories)
     .map(t => `- [${t}](#${data.technologies[t] || t.toLowerCase()})`)
@@ -44,44 +28,57 @@ const toc = Object.keys(sortedCategories)
 
 const content = Object.keys(sortedCategories)
     .map(category => {
-        const repos = sortedCategories[category].map(repo => `- [${repo.name}](${repo.link}) _(label: ${repo.label || 'n/a'})_ <br> ${repo.description}`).join('\n')
-        return `## ${category}\n\n${repos}\n`
+        const repos = sortedCategories[category]
+            .map(repo => `- [${repo.name}](${repo.link}) _(label: ${repo.label || 'n/a'})_ <br> ${repo.description}`)
+            .join('\n');
+        return `## ${category}\n\n${repos}\n`;
     }).join('\n');
 
-const sponsorList = data.sponsors.map(sponsor => `<td align="center"><a href="${sponsor.link}"><img src="${sponsor.image}" width="60px;" alt=""/><br/><sub><b>${sponsor.name}</b></sub></a></td>`)
+const sponsorList = data.sponsors.map(sponsor => 
+    `<td align="center"><a href="${sponsor.link}"><img src="${sponsor.image}" width="60px;" alt=""/><br/><sub><b>${sponsor.name}</b></sub></a></td>`
+);
 const sponsorRows = Math.ceil(sponsorList.length / 6);
 
 let sponsors = '';
 
-for (let i = 1; i <= sponsorRows; i++) {
+for (let i = 0; i < sponsorRows; i++) {
     sponsors += '<tr>';
-    for(let j = 0; j < 6; j++) {
-        if (sponsorList.length > i*j) {
-            sponsors += sponsorList[i*j];
-        } else if (sponsorRows > 1) {
-            sponsors += '<td></td>'
+    for (let j = 0; j < 6; j++) {
+        const index = i * 6 + j;
+        if (index < sponsorList.length) {
+            sponsors += sponsorList[index];
+        } else {
+            sponsors += '<td></td>';
         }
     }
     sponsors += '</tr>';
 }
 
-sponsors = `<table>${sponsors}</table>`
+sponsors = `<table>${sponsors}</table>`;
 
 saveFile(TARGET, render(tpl, { toc, content, sponsors }));
 
 function getTemplate(file) {
-    return fs.readFileSync(file).toString();
+    try {
+        return fs.readFileSync(file, 'utf8');
+    } catch (err) {
+        console.error(`Error reading template file: ${err}`);
+        process.exit(1);
+    }
 }
 
 function saveFile(file, contents) {
-    return fs.writeFileSync(file, contents);
+    try {
+        fs.writeFileSync(file, contents);
+    } catch (err) {
+        console.error(`Error writing to file: ${err}`);
+        process.exit(1);
+    }
 }
 
 function render(template, variables) {
-    Object
-        .entries(variables)
-        .forEach(([key, value]) => {
-            template = template.replace(new RegExp(`<% ${key} %>`, 'g'), value);
-        });
+    Object.entries(variables).forEach(([key, value]) => {
+        template = template.replace(new RegExp(`<% ${key} %>`, 'g'), value);
+    });
     return template;
 }
